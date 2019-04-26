2886ab9081c6c97bcb6aec264fc156126cdda094
Reynard van den Berg <reyvdb@gmail.com>
2019-04-26 14:55:32 +0200

    Make scroll_fix.js succinct

--- scroll_fix.js
+++ scroll_fix.js
@@ -1,10 +1,5 @@
-const sym = $(".gallery_symbols");
-
   $(window).on("scroll", function() {
-    console.log('Scroll event fired');
-    console.log($(window).scrollTop());
       if ($(window).scrollTop() > 185) {
-        console.log('wrap scroll event');
         $(".gallery_symbols").addClass("fix-search");
       }
       else {
