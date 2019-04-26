0b91b90183fd58b02748ccad158a015e86f0e854
Reynard van den Berg <reyvdb@gmail.com>
2019-04-26 11:25:44 +0200

    Fix colour errors

---
+++ scroll_fix.js
@@ -0,0 +1,11 @@
+// const wrap = document.querySelector('#wrap');
+//
+// wrap.addEventListener('scroll', function() {
+//   console.log('scroll');
+//   if (wrap.scrollTop > 147) {
+//     wrap.ClassList.add("fix-search");
+//     console.log('scroll-top = 147')
+//   } else {
+//     document.getElementById('gallery_symbols').ClassList.remove("fix-search");
+//   }
+// });
