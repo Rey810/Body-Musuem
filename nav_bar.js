060610f6c92eb386206dc7446484fd17e1164365
Reynard van den Berg <reyvdb@gmail.com>
2019-05-15 22:40:11 +0200

    mobile horiz scroll, filtered imgs, responsive navicon

--- nav_bar.js
+++ nav_bar.js
@@ -4,10 +4,14 @@ function myFunction() {
   if (x.className === "topnav") {
     x.className += " responsive";
     y.className += " responsive";
+    z.className += " open";
   } else {
     x.className = "topnav";
     y.className = "gallery_title_area";
+    z.classList.remove("open")
   }
 }
 
 const y = document.querySelector('.gallery_title_area');
+const z = document.querySelector('.navicon-button');
+
--- scroll_fix.js
+++ scroll_fix.js
@@ -3,6 +3,8 @@
 window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
+   let vw = window.innerWidth * 0.01;
+   document.documentElement.style.setProperty('--vw', `${vw}px`);
 });
 
 //defines position at which scroll bar gets sticky
